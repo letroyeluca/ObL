f7f3a37cbff275e3ed5084a1603b9b1588d42634
letroyeluca <luca.letroye@student.uantwerpen.be>
2025-03-20 14:11:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,14 +19,14 @@
 #include <string>
 
 struct Vertex {
-    float x, y, z;
+    double x, y, z;
 };
 
 struct Line {
     double v1, v2;
 };
 
-int main(double argc, char* argv[]) {
+int main(int argc, char* argv[]) {
     if (argc < 6) {
         std::cout << "Usage: " << argv[0] << " <input.obj> <output.ini> <eye_x> <eye_y> <eye_z>" << std::endl;
         return 0;
